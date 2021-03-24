dc4e5c34b1859d82bb572299a94c6b34ff7ee6e6
Bruno Fellipe <55933534+BrunoFellipe@users.noreply.github.com>
2021-03-24 20:06:17 -0300

    Aula 24/03

---
+++ teste.dart
@@ -0,0 +1,26 @@
+// Programa de Soma
+
+//import 'dart:io';
+
+
+void main() {
+
+double valor1 = 2;
+double valor2 = 5;
+
+double soma = valor1 + valor2;
+
+print('teste');
+
+
+
+}
+
+// Programa de Média 
+
+//double valor1 = 5
+//double valor2 = 10
+
+//double media = valor1 / valor2
+
+//print (media)
